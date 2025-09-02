3aab6c87fe45016bce33040d2982518d96a9f665
oilkcode <oilwar@163.com>
2025-09-02 16:19:13 +0800

    地图阻挡信息2

--- 01-code/01-manager/TmTilemapData.cs
+++ 01-code/01-manager/TmTilemapData.cs
@@ -1,19 +1,40 @@
 using Godot;
 using System;
+using System.Diagnostics;
 
 public partial class TmTilemapData : TileMapLayer
 {
-	public AStarGrid2D astar = new AStarGrid2D();
-
     public override void _Ready()
     {
-        Rect2i UsedRect = get_used_rect();
-        Vector2i tilemap_size = UsedRect.end - UsedRect.position;
-        Rect2i map_rect = new Rect2i(Vector2i.ZERO, tilemap_size);
-        Vector2i tile_size = get_tile_set().tile_size;
+        Rect2I UsedRect = GetUsedRect();
+        Vector2I tilemap_size = UsedRect.End - UsedRect.Position;
+        Rect2I map_rect = new Rect2I(Vector2I.Zero, tilemap_size);
+        Vector2I tile_size = GetTileSet().TileSize;
+        Vector2 TileSize;
+        TileSize.X = tile_size.X * Scale.X;
+        TileSize.Y = tile_size.Y * Scale.Y;
 
-        astar.region = map_rect;
-        astar.cell_size = tile_size;
+        GD.Print(UsedRect.ToString());
+        GD.Print(TileSize.ToString());
+
+        Vector2I _Index = new Vector2I();
+        for (int _y = 0; _y < tilemap_size.Y; ++_y)
+        {
+            for (int _x = 0; _x < tilemap_size.X; ++_x)
+            {
+                _Index.X = UsedRect.Position.X + _x;
+                _Index.Y = UsedRect.Position.Y + _y;
+                TileData SingleTileData = GetCellTileData(_Index);
+                if (SingleTileData != null)
+                {
+                    if (SingleTileData.HasCustomData("block_flag"))
+                    {
+                        int FlagValue = SingleTileData.GetCustomData("block_flag").AsInt32();
+                        GD.Print(_Index.ToString() + "  " + FlagValue.ToString());
+                    }
+                }
+            }
+        }
 	}
 	
 }
